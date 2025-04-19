508f91796ae04594982a9d5965357c157cdd3d02
Nico <87032389+nicorec21@users.noreply.github.com>
2025-04-19 12:45:23 -0300

    subo guia asm clonada

---
+++ Guia_asm/guia-ASM/src/2 - ABI/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <assert.h>
+
+#include "../test-utils.h"
+#include "ABI.h"
+
+int main() {
+	/* Acá pueden realizar sus propias pruebas */
+	assert(alternate_sum_4_using_c(8, 2, 5, 1) == 6);
+
+	assert(alternate_sum_8(8,2,5,1,8,2,5,1) == 20);
+
+	assert(alternate_sum_4_using_c_alternative(8, 2, 5, 1) == 6);
+	return 0;
+}
