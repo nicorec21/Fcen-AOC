2dd826579086a744ee018ab9745d954d995c7b3a
Nico rec <nicolas.recchini@gmail.com>
2025-04-26 16:23:27 -0300

    avances

--- Parciales_viejos/2023C1P/solucion/ej1/ej1.asm
+++ Parciales_viejos/2023C1P/solucion/ej1/ej1.asm
@@ -1,15 +1,73 @@
 global templosClasicos
 global cuantosTemplosClasicos
 
-
-
 ;########### SECCION DE TEXTO (PROGRAMA)
 section .text
-;uint32_t cuantosTemplosClasicos_c(templo *temploArr, size_t=64bits temploArr_len){
+
+;templo* templosClasicos_c(templo *temploArr, size_t temploArr_len)
 ;rdi->*temploArr
 ;rsi->temploArr_len
 templosClasicos:
-    ;prologo:
+    .prologo:
+        push rbp
+        mov rbp, rsp
+
+
+    .preciclo:
+        push rdi
+        push rsi ;preservo los parametros originales antes del call (pila alineada)
+
+        call cuantosTemplosClasicos ;los parametros ya estan pasados ya q son los mismos
+        ;en rax tengo el resultado
+
+        mov rdi, rax ;rdi tiene la ctad de templos clasicos
+        imul rdi, 24 ;multiplico los clasicos x 24bytes (calculando la cantidad de espacio para pedirle al malloc)
+
+        call malloc ;el parametro ya esta en rdi (24*clasicos)
+        ;en rax tengo el resultado
+
+        pop rsi
+        pop rdi ;restauro los params originales
+
+        xor r9, r9; i=0
+        xor r10, r10; j=0
+
+    .ciclo:
+        cmp r9, rsi ; i ==? temploArr_len
+        je .epilogo 
+
+        xor rcx, rcx
+        xor rdx, rdx
+
+        mov cl, [rdi] ; cl=temploarr[i].clarga
+        mov dl, [rdi + 16] ;dl=temploarr[i].ccorta
+
+        inc r9 ;i++
+        add rdi, 24 ;paso al sig templo
+
+    .cuenta:
+        shl dl, 1 ;dl=temploarr[i].ccorta * 2
+        add dl, 1 ;dl=temploarr[i].ccorta + 1
+
+        cmp cl, dl ;cl == ? dl
+        jne .ciclo
+
+        mov rcx, [rdi] ;rcx=temploArr[i]
+        mov [rax + r10 * 24], rcx  ;templos_clasicos_arr[j] = temploArr[i];
+        inc r10 ;j++
+        jmp .ciclo ;y vuelvo al ciclo
+
+     .epilogo:
+        pop rbp
+        ret
+
+   
+
+;uint32_t cuantosTemplosClasicos_c(templo *temploArr, size_t=64bits temploArr_len){
+;rdi->*temploArr
+;rsi->temploArr_len
+cuantosTemplosClasicos:
+    .prologo:
         push rbp
         mov rbp, rsp
 
@@ -17,21 +75,22 @@ templosClasicos:
         xor r9, r9; i = 0
 
     .ciclo:
-        cmp r9, rsi ; i <? temploArr_len
-        jl .epilogo 
+        cmp r9, rsi ; i ==? temploArr_len
+        je .epilogo 
 
         xor rcx, rcx
         xor rdx, rdx
+
         mov cl, [rdi] ; cl=temploarr[i].clarga
         mov dl, [rdi + 16] ;dl=temploarr[i].ccorta
 
         inc r9 ;i++
         add rdi, 24 ;paso al sig templo
 
-    ;cuenta:
-
-        add dl, 1 ;dl=temploarr[i].ccorta + 1
+    .cuenta:
         shl dl, 1 ;dl=temploarr[i].ccorta * 2
+        add dl, 1 ;dl=temploarr[i].ccorta + 1
+
         cmp cl, dl ;cl == ? dl
         jne .ciclo
 
@@ -41,9 +100,3 @@ templosClasicos:
     .epilogo:
         pop rbp
         ret
-
-
-
-cuantosTemplosClasicos:
-
-
